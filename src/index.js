8fccebc029bc8f34ed1ae2ca121c49f4d45acbff
Grace Wang <gracewang92@gmail.com>
2021-12-05 10:57:18 -0800

    move new order component into a separate file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Button, Container, Form } from 'react-bootstrap';
 import ReactDOM from 'react-dom';
 import Web3 from 'web3';
 import './index.css';
+import { NewOrder } from './newOrder';
 
 function GasEstimator() {
   const web3 = new Web3(Web3.givenProvider);
@@ -51,7 +52,7 @@ function GasEstimator() {
         <Form className="gas-form">
           {!connected && <Wallet onClick={() => connectWalletClick()} ></Wallet>}
           {connected && <div>Wallet Connected</div>}
-          <UserInput />
+          <NewOrder />
         </Form>
       </Container>
     </div>
@@ -66,60 +67,6 @@ function Wallet(props) {
     </Form.Group>
   )
 }
-
-function UserInput() {
-  const [destinationWallet, setDestinationWallet] = React.useState('');
-  const [gasLimitDollars, setGasLimitDollars] = React.useState(0);
-  const [tokenAmount, setTokenAmount] = React.useState(0);
-  const [timeLimitHours, setTimeLimitHours] = React.useState(24);
-
-  async function addOrder() {
-    await fetch('/add-order', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        destination_wallet: destinationWallet,
-        amount: tokenAmount,
-        token: "wETH",
-        gas_limit_dollars: gasLimitDollars,
-        expires_at: new Date(new Date().getTime() + timeLimitHours * 60 * 60 * 1000).toISOString(),
-      }),
-    });
-  }
-
-  return (
-    <div>
-      <Form.Group className="mb-3">
-        <Form.Text className="text-muted">To: </Form.Text>
-        <Form.Control type="text" placeholder="Provide public key" value={destinationWallet} onChange={e => setDestinationWallet(e.target.value)} />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Text className="text-muted">Gas Limit (in USD) </Form.Text>
-        <Form.Control type="number" min="0" placeholder="" value={gasLimitDollars} onChange={e => setGasLimitDollars(Number(e.target.value))} />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Text className="text-muted">wETH tokens </Form.Text>
-        <Form.Control type="number" min="0" placeholder="" value={tokenAmount} onChange={e => setTokenAmount(Number(e.target.value))} />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Text className="text-muted">Time limit (in hours) </Form.Text>
-        <Form.Control type="number" min="0" placeholder="" value={timeLimitHours} onChange={e => setTimeLimitHours(Number(e.target.value))} />
-      </Form.Group>
-
-      <Button variant="primary" type="button" onClick={addOrder}>Submit</Button>
-    </div>
-  )
-}
-
-
-// input fields
-// From: <connect wallet>, to: <address>
-// Gas Limit: <in USD> 
-// Amount to buy: <in USD>
-// Currency: Ether
-// Time: 24 hours / 48 hours
 
 ReactDOM.render(
   <GasEstimator />,
